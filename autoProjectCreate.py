ff2db8f5b657a79b551f9fd839bc8780eece2091
maheshgopi08 <mahesh.gopi08@gmail.com>
2019-11-15 10:42:27 +0000

    creating folders in respective directories

--- autoProjectCreate.py
+++ autoProjectCreate.py
@@ -6,7 +6,31 @@ import os
 
 #creating a folder in the respective project directories
 
-dir= "c:/users/administrator/documents" #Defeault directory to store the differerent projects
+#path= "c:/users/administrator/documents/" #Defeault directory to store the differerent projects
+
+username = "maheshgopi08" #Insert your github username here
+password = "" #Insert your github password here
 
 
-print((sys.argv[1:]))
+
+def create():
+    
+    print((sys.argv[1:]))
+    folderName = str(sys.argv[1])
+    if("py" in folderName ):    #Condition for python
+        path="c:/users/administrator/documents/python/"
+    if("fl" in folderName ):    #Condition for flutter
+        path="C:/src/projects/"
+    
+    
+
+    
+    os.makedirs(path + str(folderName))
+
+    #user = Github(username, password).get_user()
+    #repo = user.create_repo(folderName)
+    #print("Succesfully created repository {}".format(folderName))
+
+
+if __name__ == "__main__":
+    create()
