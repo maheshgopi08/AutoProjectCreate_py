94b16122c6e6924da0c5b50b5bcabb6eeca1ce14
maheshgopi08 <mahesh.gopi08@gmail.com>
2019-11-15 06:11:39 +0000

    initial commit+reading sys arguments

---
+++ autoProjectCreate.py
@@ -0,0 +1,12 @@
+#To automate the project creation
+
+import sys
+import os
+#from github import Github
+
+#creating a folder in the respective project directories
+
+dir= "c:/users/administrator/documents" #Defeault directory to store the differerent projects
+
+
+print((sys.argv[1:]))
